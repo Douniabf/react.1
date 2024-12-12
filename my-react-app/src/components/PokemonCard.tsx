732c7888c39ecfc6983a8d4cd4c808d27d515cb3
Douniabf <bouhayouf-dounia@hotmail.com>
2024-12-12 16:43:17 +0100

    add pokemon mew

--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,13 +1,30 @@
+import React from 'react';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function PokemonCard() {
-    return(
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Cette image représente le Pokémon Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
-        </figure>
-    )
+  // Sélection du deuxième Pokémon ("mew")
+  const pokemon = pokemonList[1];
 
+  return (
+    <figure>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={`Cette image représente le Pokémon ${pokemon.name}`} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
 }
 
 export default PokemonCard;
